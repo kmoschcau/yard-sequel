56dada97b8213e2da1b1dcbd9b7e0c23c483eec2
Kai Moschcau <kai.moschcau@gmail.com>
2017-03-12 14:02:50 +0100

    moves handlers to gem namespace

--- lib/yard-sequel/associations/many_to_one_handler.rb
+++ lib/yard-sequel/associations/many_to_one_handler.rb
@@ -1,27 +1,19 @@
 # frozen_string_literal: true
-module YARD
-  module Handlers
-    module Ruby
-      module Sequel
-        module Associations
-          # The handler class for Sequel many_to_one associations.
-          # @author Kai Moschcau
-          class ManyToOneHandler < YardSequel::Associations::AssociationHandler
-            include YardSequel::Associations::DatasetMethod
-            include YardSequel::Associations::ToOneMethods
 
-            handles method_call(:many_to_one)
-            namespace_only
-
-            def process
-              super
-              create_to_one_getter
-              create_to_one_setter
-
-              create_dataset_method
-            end
-          end
-        end
+module YardSequel
+  module Associations
+    # The handler class for Sequel many_to_one associations.
+    # @author Kai Moschcau
+    class ManyToOneHandler < YardSequel::Associations::AssociationHandler
+      include YardSequel::Associations::DatasetMethod
+      include YardSequel::Associations::ToOneMethods
+      handles method_call(:many_to_one)
+      namespace_only
+      def process
+        super
+        create_to_one_getter
+        create_to_one_setter
+        create_dataset_method
       end
     end
   end
--- lib/yard-sequel/associations/one_to_many_handler.rb
+++ lib/yard-sequel/associations/one_to_many_handler.rb
@@ -1,29 +1,21 @@
 # frozen_string_literal: true
-module YARD
-  module Handlers
-    module Ruby
-      module Sequel
-        module Associations
-          # The handler class for Sequel one_to_many associations.
-          # @author Kai Moschcau
-          class OneToManyHandler < YardSequel::Associations::AssociationHandler
-            include YardSequel::Associations::DatasetMethod
-            include YardSequel::Associations::ToManyMethods
 
-            handles method_call(:one_to_many)
-            namespace_only
-
-            def process
-              super
-              create_to_many_adder
-              create_to_many_clearer
-              create_to_many_getter
-              create_to_many_remover
-
-              create_dataset_method
-            end
-          end
-        end
+module YardSequel
+  module Associations
+    # The handler class for Sequel one_to_many associations.
+    # @author Kai Moschcau
+    class OneToManyHandler < YardSequel::Associations::AssociationHandler
+      include YardSequel::Associations::DatasetMethod
+      include YardSequel::Associations::ToManyMethods
+      handles method_call(:one_to_many)
+      namespace_only
+      def process
+        super
+        create_to_many_adder
+        create_to_many_clearer
+        create_to_many_getter
+        create_to_many_remover
+        create_dataset_method
       end
     end
   end

b3803994104a3f8ae826e02c358e3c7ee5030c0e
Kai Moschcau <kai.moschcau@sabre.com>
2017-05-22 15:30:29 +0200

    removes accidentally added line

--- lib/yard-sequel/associations.rb
+++ lib/yard-sequel/associations.rb
@@ -8,7 +8,6 @@ end
 
 require_relative 'associations/association_handler'
 require_relative 'associations/modules'
-require_relative 'associations/association_options'
 
 require_relative 'associations/many_to_one_handler'
 require_relative 'associations/one_to_many_handler'
